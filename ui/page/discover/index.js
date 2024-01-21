b29a0a1b941fc81108f80a9bdcfa22d2b7f68066
MG <53813521+meneergroot@users.noreply.github.com>
2024-01-21 15:06:13 +0000

    0.1

---
+++ ui/page/discover/index.js
@@ -0,0 +1,27 @@
+import * as CS from 'constants/claim_search';
+import { connect } from 'react-redux';
+import { doResolveUri } from 'redux/actions/claims';
+import { selectClaimForUri } from 'redux/selectors/claims';
+import * as SETTINGS from 'constants/settings';
+import { selectClientSetting } from 'redux/selectors/settings';
+import { selectUserHasOdyseePremiumPlus } from 'redux/selectors/memberships';
+import DiscoverPage from './view';
+
+const select = (state, props) => {
+  const urlParams = new URLSearchParams(props.location.search);
+  const repostedUriInUrl = urlParams.get(CS.REPOSTED_URI_KEY);
+  const repostedUri = repostedUriInUrl ? decodeURIComponent(repostedUriInUrl) : undefined;
+
+  return {
+    repostedUri: repostedUri,
+    repostedClaim: repostedUri ? selectClaimForUri(state, repostedUri, false) : null,
+    tileLayout: selectClientSetting(state, SETTINGS.TILE_LAYOUT),
+    hasPremiumPlus: selectUserHasOdyseePremiumPlus(state),
+  };
+};
+
+const perform = {
+  doResolveUri,
+};
+
+export default connect(select, perform)(DiscoverPage);
---
+++ ui/page/fileListPublished/index.js
@@ -0,0 +1,41 @@
+import { connect } from 'react-redux';
+import {
+  selectIsFetchingClaimListMine,
+  selectMyClaimsPage,
+  selectMyClaimsPageItemCount,
+  selectFetchingMyClaimsPageError,
+  selectMyChannelClaimIds,
+} from 'redux/selectors/claims';
+import { selectUploadCount } from 'redux/selectors/publish';
+import { doFetchClaimListMine, doCheckPendingClaims, doClearClaimSearch } from 'redux/actions/claims';
+import { doBeginPublish } from 'redux/actions/publish';
+import FileListPublished from './view';
+import { withRouter } from 'react-router';
+import { MY_CLAIMS_PAGE_SIZE, PAGE_PARAM, PAGE_SIZE_PARAM } from 'constants/claim';
+
+const select = (state, props) => {
+  const { search } = props.location;
+  const urlParams = new URLSearchParams(search);
+  const page = Number(urlParams.get(PAGE_PARAM)) || '1';
+  const pageSize = urlParams.get(PAGE_SIZE_PARAM) || String(MY_CLAIMS_PAGE_SIZE);
+
+  return {
+    page,
+    pageSize,
+    fetching: selectIsFetchingClaimListMine(state),
+    urls: selectMyClaimsPage(state),
+    urlTotal: selectMyClaimsPageItemCount(state),
+    error: selectFetchingMyClaimsPageError(state),
+    uploadCount: selectUploadCount(state),
+    myChannelIds: selectMyChannelClaimIds(state),
+  };
+};
+
+const perform = {
+  checkPendingPublishes: doCheckPendingClaims,
+  fetchClaimListMine: doFetchClaimListMine,
+  doClearClaimSearch,
+  doBeginPublish,
+};
+
+export default withRouter(connect(select, perform)(FileListPublished));
